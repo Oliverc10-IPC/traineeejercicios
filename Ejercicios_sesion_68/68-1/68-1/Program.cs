8f3c51dafe949bd4a91d054783d32b835720bf5c
Oliver Cruz <oliver44681598@gmail.com>
2023-05-12 10:28:44 -0600

    Se termino el ejercicio 68-1

---
+++ Ejercicios_sesion_68/68-1/68-1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using static System.Console;
+
+
+
+namespace Validacion_Alejandro // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido");
+            WriteLine();
+            WriteLine("Por favor ingresa tu nombre para ver si te conozco:");
+            var nombre = ReadLine().ToLower();
+            nombre = nombre.Trim();
+            if (nombre.Equals("alejandro"))
+            {
+                Console.WriteLine("Hola");
+
+            }
+            else { 
+                WriteLine("No te conozco");  
+            }
+
+        }
+    }
+}
