c922a1f7f932f67502d3795f6a24ea11c1d5e1d4
Oliver Cruz <oliver44681598@gmail.com>
2023-05-18 23:14:55 -0600

    Se termino el reto de la Semana 3

---
+++ Semana_3/Listado/Listado/Alumno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alumnos
+
+{
+    internal class Alumno
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Id { get; set; }
+        public string Carrera { get; set; }
+        public List<double> Notas { get; set; }
+    
+        public Alumno() { 
+            Nombre = string.Empty;
+            Apellido = string.Empty;
+            Id = 0;
+            Carrera = string.Empty;
+            Notas = new List<double>();      
+        
+        }
+        public Alumno(string nombre, string apellido, int id, string carrera, List<double> notas)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Id = id;
+            Carrera = carrera;
+            Notas = notas;
+        }
+        public double promedioNotas(List<double> notas) {
+            double notasSumadas = 0;
+            double contador = 0;
+            foreach (double nota in notas) { 
+                notasSumadas += nota;
+                contador++;
+            }
+            return notasSumadas / contador; 
+        
+        }
+      
+    }
+
+}
---
+++ Semana_3/Listado/Listado/Program.cs
@@ -0,0 +1,96 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using static System.Console;
+using Alumnos;
+
+namespace MenuPrincipal // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Alumno> Alumnos = new List<Alumno>();
+            Alumno alumno1 = new Alumno("Alina ", "Falcon", 1, "Ingenieria en Electrinica", generadorNotas());
+            Alumno alumno2 = new Alumno("Oier ", "Llorens", 2, "Ingenieria en Electrinica", generadorNotas());
+            Alumno alumno3 = new Alumno("Iago  ", "Arteaga", 3, "Ingenieria en Electrinica", generadorNotas());
+            Alumno alumno4 = new Alumno("Francisco  ", "Paredes", 4, "Ingenieria en Electrinica", generadorNotas());
+            Alumno alumno5 = new Alumno("Manel  ", "Manzano", 5, "Ingenieria en Electrinica", generadorNotas());
+            Alumnos.Add(alumno1);
+            Alumnos.Add(alumno2);
+            Alumnos.Add(alumno3);
+            Alumnos.Add(alumno4);
+            Alumnos.Add(alumno5);
+            MenuPrincipal(Alumnos);
+
+
+
+
+
+        }
+        public static void MenuPrincipal(List<Alumno> Alumnos)
+        {
+            WriteLine("Bienvenido");
+            WriteLine();
+            WriteLine("1. Ver listado completo");
+            WriteLine("2. Ver alumnos aprobados.");
+            WriteLine();
+            WriteLine("Por favor escoge una opcion: ");
+            int opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    impresionListadoCompleto(Alumnos);
+                    WriteLine();
+                    MenuPrincipal(Alumnos);
+                    break;
+                case 2:
+                    impresionListadoAprobados(Alumnos);
+                    WriteLine();
+                    MenuPrincipal(Alumnos); 
+                    break;
+                default:
+
+                    WriteLine("Por favor escoja una opcion correcta");
+                    MenuPrincipal(Alumnos);
+                    break;
+            }
+        }
+
+            public static List<double> generadorNotas()
+            {
+                List<double> notas = new List<double>();
+                Random rnd = new Random();
+                int numeroNotas = rnd.Next(5, 10 + 1);
+                for (int i = 0; i < numeroNotas; i++)
+                {
+                    notas.Add(rnd.Next(1, 10 + 1));
+
+                }
+
+                return notas;
+
+            }
+            public static void impresionListadoCompleto(List<Alumno> alumnos)
+            {
+                foreach (var alumno in alumnos)
+                {
+                    WriteLine("-------------------------------------------");
+                    WriteLine($"Nombre del Alumno: {alumno.Nombre} {alumno.Apellido}\nID: {alumno.Id}\nCarrera: {alumno.Carrera}\nPromedio de Notas: {alumno.promedioNotas(alumno.Notas)}");
+                }
+
+            }
+            public static void impresionListadoAprobados(List<Alumno> alumnos)
+            {
+                foreach (var alumno in alumnos)
+                {
+                    if (alumno.promedioNotas(alumno.Notas) >= 6)
+                    {
+                        WriteLine("-------------------------------------------");
+                        WriteLine($"Nombre del Alumno: {alumno.Nombre} {alumno.Apellido}\nID: {alumno.Id}\nPromedio de Notas: {alumno.promedioNotas(alumno.Notas)}");
+                    }
+
+                }
+
+            }
+        }
+    }
