d680af3e24b3be4a29904094206c7ce713ac8a72
Oliver Cruz <oliver44681598@gmail.com>
2023-05-24 23:35:53 -0600

    Se crearon todas las clases y se hicieron pruebas del funcionamiento

---
+++ Semana_4/Semana_4/Semana_4/Program.cs
@@ -0,0 +1,134 @@
+﻿
+using System.Linq;
+using static System.Console;
+
+namespace Biblioteca // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Biblioteca biblioteca = new Biblioteca();
+            Libro libro1 = new Libro("Pinoccio", "Carlo Collodi");
+            Revista revista1 = new Revista("Time","2da");
+            biblioteca.AgregarMaterial(libro1);
+            biblioteca.AgregarMaterial(revista1);
+            List<Material> opciones = biblioteca.BuscarPorTitulos("Pino");
+            foreach (Material mat in opciones) { 
+                    WriteLine(mat.nombre); 
+            }
+            
+            
+        }
+    }
+
+    public abstract class Material
+    {
+        public String nombre { set; get; }
+        public bool disponible { set; get; }
+
+
+
+    }
+    public class Libro : Material
+    {
+
+        String Autor { set; get; }
+
+              
+        public Libro(String nombre, String Autor)
+        {
+            this.nombre = nombre;
+            this.Autor = Autor;
+            this.disponible = true;
+        }
+
+        public void Prestar()
+        {
+            this.disponible = false;
+
+        }
+        public void Devolver()
+        {
+            this.disponible = true;
+
+        }
+    }
+    public class Revista : Material
+    {
+
+        String Edicion { set; get; }
+
+
+       
+
+        public Revista(String nombre, String Edicion)
+        {
+            this.nombre = nombre;
+            this.Edicion = Edicion;
+            this.disponible = true;
+        }
+
+        public void Prestar()
+        {
+            this.disponible = false;
+
+        }
+
+    }
+    interface IBiblioteca
+    {
+
+        public void AgregarMaterial(Material material);
+        public List<Material> BuscarPorTitulos(string titulo);
+        public void PrestarMaterial(Material material);
+        public void DevolverMaterial(Material material);
+
+
+    }
+    public class Biblioteca : IBiblioteca
+    {
+        List<Material> lbiblioteca { set; get; }
+
+        public Biblioteca() { 
+        lbiblioteca = new List<Material>();  
+        
+        }
+        public void AgregarMaterial(Material material)
+        {
+              
+            lbiblioteca.Add(material);   
+            
+        }
+
+        public List<Material> BuscarPorTitulos(string titulo)
+        {
+            List <Material> opcionesPosibles = new List<Material>();    
+            foreach (Material mat in lbiblioteca)
+            {
+                if (mat.nombre.Contains(titulo)) { 
+                    opcionesPosibles.Add(mat);
+                }
+            }
+            return opcionesPosibles;
+            
+        }
+
+        public void DevolverMaterial(Material material)
+        {
+            material.disponible = true;
+        }
+
+        public void PrestarMaterial(Material material)
+        {
+            if (material.disponible)
+            {
+                material.disponible = false;
+            }
+            else {
+                WriteLine("El libro no se encuentra disponible");
+                
+            }
+        }
+    }
+}
