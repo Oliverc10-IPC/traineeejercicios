fb700591413f47a63681deaaf9a7c51e2f013e6b
Oliver Cruz <oliver44681598@gmail.com>
2023-05-28 23:17:42 -0600

    Se termino el ejercicio 108

---
+++ Ejercicios_sesion_108/108/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal perro = new Animal();
+            perro.Andar();
+            perro.Saltar();
+
+        }
+    }
+
+    interface IAnimal {
+
+        public void Andar();
+        public bool esPerro();
+        public void Saltar();
+    
+    }
+
+    public class Animal : IAnimal
+    {
+
+        public void Andar()
+        {
+            Console.WriteLine("Andando");
+        }
+        public bool esPerro()
+        {
+
+            Console.WriteLine("Es perro");
+                return true;
+        }
+        public void Saltar() {
+            Console.WriteLine("Saltando");
+        
+        }
+    }
+}
